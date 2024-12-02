ed21a67c4aa79aaa60e84540f1dd9237af03df8b
arnaudprc <arnaud.preci@gmail.com>
2024-12-02 20:38:23 +0100

    Refactor main function indentation and formatting in deploiement.py

--- deploiement.py
+++ deploiement.py
@@ -1,31 +1,58 @@
-import inquirer
-import os
+import paramiko
+
+def ssh_connect(hostname, port, username, password):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname, port, username=username, password=password)
+    return client
+
+def install_package(client, package_name):
+    stdin, stdout, stderr = client.exec_command(f'sudo apt-get install -y {package_name}')
+    print(stdout.read().decode())
+    print(stderr.read().decode())
+
+def configure_package(client, config_command):
+    stdin, stdout, stderr = client.exec_command(config_command)
+    print(stdout.read().decode())
+    print(stderr.read().decode())
 
 def main():
-    questions = [
-        inquirer.List('choice',
-                      message="What do you want to do?",
-                      choices=['Connect to ssh','Install packages', 'Configure the service','Back', 'Quit'],
-                      ),
-    ]
-    answers = inquirer.prompt(questions)
-    print(f"You chose: {answers['choice']}")
+    hostname = input("Entrez l'adresse IP de la machine distante : ")
+    port = 22
+    username = input("Entrez votre nom d'utilisateur : ")
+    password = input("Entrez votre mot de passe : ")
+
+    client = ssh_connect(hostname, port, username, password)
+
+    while True:
+        print("\nMenu:")
+        print("1. Installer un package")
+        print("2. Désinstaller un package")
+        print("3. Configurer un package")
+        print("4. Quitter")
+        choice = input("Choisissez une option : ")
+
+        if choice == '1':
+            package_name = input("Entrez le nom du package à installer : ")
+            install_package(client, package_name)
+
+        elif choice == '2':
+            package_name = input("Entrez le nom du package à désinstaller : ")
+            stdin, stdout, stderr = client.exec_command(f'sudo apt-get remove -y {package_name}')
+            print(stdout.read().decode())
+            print(stderr.read().decode())
+
+        elif choice == '3':
+            config_command = input("Entrez la commande de configuration : ")
+            configure_package(client, config_command)
+
+        elif choice == '4':
+            break
         
-    if answers['choice'] == 'Install packages':
-        package_name = input("Enter the package name to install: ")
-        os.system(f"sudo apt-get install -y {package_name}")
-        print(f"Package {package_name} installed successfully")
+        else:
+            print("Option invalide, veuillez réessayer.")
 
-    if answers['choice'] == 'Configure the service':
-        service_name = input("Enter the service name to configure: ")
-        os.system(f"sudo systemctl start {service_name}")
-        print(f"Service {service_name} started successfully")
-
-    if answers['choice'] == 'Back':
-        main()
-
-    if answers['choice'] == 'Quit':
-        return
+    client.close()
 
 if __name__ == "__main__":
     main()
