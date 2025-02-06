0f5988ba08f8eb1b693755298a8b39ee0c4ca917
arnaudprc <arnaud.preci@gmail.com>
2025-02-06 08:48:18 +0100

    Add run_command function to execute commands with sudo privileges in ldap.py

---
+++ ldap.py
@@ -0,0 +1,14 @@
+def run_command(client, command, sudo_password, description):
+    print(f"[INFO] {description}...")
+    stdin, stdout, stderr = client.exec_command(f"echo {sudo_password} | sudo -S {command}", get_pty=True)
+    output = stdout.read().decode()
+    error = stderr.read().decode()
+    if output:
+        print(output)
+    if error:
+        print(f"[ERROR] {description}. Error: {error}")
+    else:
+        print(f"[SUCCESS] {description}.")
+
+def ldap():
+    
